661ef1a30e9a12cc1b3d08562961a673c264a636
Sai Nishanth <57940090+SaiNishanth-cloud@users.noreply.github.com>
2023-03-05 16:10:33 +0100

    Update cluster.tf
    Added Comments

--- cluster.tf
+++ cluster.tf
@@ -1,12 +1,12 @@
 provider "google" {
-  project = "kubernetes-cluster-379315"
-  region  = "us-central1-a"
+  project = "kubernetes-cluster-379315" #Provide the project name
+  region  = "us-central1-a" #Provide a region where to deploy
   credentials = file("./credentials.json")
 }
 
 resource "google_container_cluster" "primary" {
-  name               = "my-kube-cluster"
-  location           = "us-central1-a"
+  name               = "my-kube-cluster" #Provide the cluster a name
+  location           = "us-central1-a"  #Provide a location where to deploy the cluster
   initial_node_count = 1
 
   node_config {
